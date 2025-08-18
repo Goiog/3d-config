900a492d28c7ce358574adf91a8657a97a14c347
Goiog <95105335+Goiog@users.noreply.github.com>
2025-08-18 15:32:07 +0200

    Update Shirt.jsx

--- src/components/Shirt.jsx
+++ src/components/Shirt.jsx
@@ -28,8 +28,13 @@ export function Shirt(props) {
         <mesh name="Object_7"  geometry={nodes.Object_7.geometry} material={materials.Material_001} />
         <mesh name="Object_8"  geometry={nodes.Object_8.geometry} material={materials.Material_002} />
         <mesh name="Object_9"  geometry={nodes.Object_9.geometry} material={materials.Material_003} />
-        <mesh name="Object_6" geometry={nodes.Object_6.geometry} material={materials.Tshirtmaterialout} ><CanvasTexture />
-          </mesh>
+        const canvas = document.createElement("canvas")
+        const ctx = canvas.getContext("2d")
+        const texture = new THREE.CanvasTexture(canvas)
+        
+        <mesh name="Object_6" geometry={nodes.Object_6.geometry}>
+          <meshStandardMaterial map={texture} />
+        </mesh>
       </group>
     </group>
   );
