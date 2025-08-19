ea16097648e760b91add8374cf75ef0b94802473
Goiog <95105335+Goiog@users.noreply.github.com>
2025-08-19 08:33:29 +0200

    Update ModelWrapper.jsx

--- src/components/ModelWrapper.jsx
+++ src/components/ModelWrapper.jsx
@@ -175,7 +175,7 @@ const ModelWrapper = ({ Model, cameraRef, orbitRef }) => {
     function getRealPosition(axis, value) {
       let CORRECTION_VALUE = axis === "x" ? 4.5 : 5.5;
 
-      return Math.round(value * 100);
+      return Math.round(value * 2700);
     }
 
     var getMousePosition = function (dom, x, y) {
@@ -280,4 +280,5 @@ const CanvasTexture = React.memo(({ flip }) => {
 });
 
 export { CanvasTexture };
+
 
