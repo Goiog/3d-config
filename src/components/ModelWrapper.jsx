cb49742f98183ff2e9c5ffa934106ffda9aae7e9
Goiog <95105335+Goiog@users.noreply.github.com>
2025-08-19 18:27:25 +0200

    Update ModelWrapper.jsx

--- src/components/ModelWrapper.jsx
+++ src/components/ModelWrapper.jsx
@@ -213,8 +213,8 @@ const ModelWrapper = ({ Model, cameraRef, orbitRef }) => {
     //const cropH = 812;
     const cropX = 0;
     const cropY = 0;
-    const cropW = 1066;
-    const cropH = 1082;
+    const cropW = 1100;
+    const cropH = 1100;
     // Target aspect ratio (8:4)
     const targetW = 800;
     const targetH = 400;
@@ -368,6 +368,7 @@ const CanvasTexture = React.memo(({ flip }) => {
 });
 
 export { CanvasTexture };
+
 
 
 
