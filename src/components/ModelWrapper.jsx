726aec0914c1d208740868ac8a2ab7d4960faac9
Goiog <95105335+Goiog@users.noreply.github.com>
2025-08-23 08:09:40 +0200

    Update ModelWrapper.jsx

--- src/components/ModelWrapper.jsx
+++ src/components/ModelWrapper.jsx
@@ -237,17 +237,7 @@ switch (selectedModel.current) {
   targetH = Math.round(800 / aspect);
   break;
   case "Shirt": targetW = 600; targetH = 300; break;
-  case "Cap":
-  const aspectCap = el.width / el.height;
-  const side = Math.min(canvasW, canvasH); // ensure square
-  cropX = (canvasW - side) / 2; // center horizontally
-  cropY = (canvasH - side) / 2; // center vertically
-  cropW = side;
-  cropH = side;
-
-  targetW = 800;
-  targetH = 800; // force square for cap
-  break;
+  case "Cap":targetW = 800; targetH = 400; break;
 
 
   case "Poster": targetW = 1000; targetH = 800; break;
@@ -410,6 +400,7 @@ const CanvasTexture = React.memo(({ flip }) => {
 });
 
 export { CanvasTexture };
+
 
 
 
